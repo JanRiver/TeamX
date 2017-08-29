e78d398238d41073622abe76ce39bfb0a35df54e
Nguyen Nhu Vu <vunguyenjr@gmail.com>
2017-08-29 21:18:18 +0700

    Update price and price chart

--- FB.cpp
+++ FB.cpp
@@ -7,22 +7,18 @@ using namespace std;
 
 //global variable
 
-//amount of fruits in inventory
-float   inv_apple = 0,
-        inv_banana = 0,
-        inv_grape = 0,
-        inv_orange = 0,
-        inv_watermelon = 0;
-
-    float cash = 11000;
-    float day = 0;
-    float networth = 11000;
+    float cash = 100;
+    int day = 1;
+    float networth = 100;
 
 struct Fruit
 {
     string name;
     float price;
     float wastage;
+    float amount;
+    char lastPrices[6];
+    float months[12];
 };
 
 
@@ -33,7 +29,7 @@ void printPrice(Fruit fruit[], int n);
 
 //print everything in inventory, fruits, cash,...
 //assigned to Nhan
-void printInventory();
+void printInventory(Fruit *fruit, int array_size);
 
 //buy fruit
 bool buy(Fruit* fruit, int array_size);
@@ -48,34 +44,36 @@ char getChar(string question);
 //print a question and get an integer as answer
 int getInt(string question);
 
+float getFloat(string question);
+
 //update information after a day
-void update();
+void update(Fruit *fruit, int array_size);
 
 //get a random number
 //assigned to Vang Vang
 float getRandom();
 
+string getMonthString(int month_as_number);
+void printScreen(Fruit *fruit, int array_size);
+
+void printResult();
+
 
 int main()
 {
-    Fruit fruit[5] {
-     { "Apple", 4.5, 1.0 },
-     { "Banana", 2.5, 1.8 },
-     { "Grape", 9.0, 2.0},
-     { "Orange", 4.0, 1.0},
-     { "Watermelon", 1.00, 1.2},
+    Fruit fruit[5] = {
+     { "Apple",      4.5, 1.0, 0.0, ".....", {7, 6, 5, 4, 3, 2, 3, 4, 5, 6, 7, 8} },
+     { "Banana",     2.5, 1.8, 0.0, ".....", {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5} },
+     { "Grape",      9.0, 2.0, 0.0, ".....", {4, 3, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5} },
+     { "Orange",     4.0, 1.0, 0.0, ".....", {5, 5, 5, 2, 2, 2, 5, 5, 8, 8, 8, 5} },
+     { "Watermelon", 1.0, 1.2, 0.0, ".....", {1, 2, 3, 7, 8, 9, 8, 7, 5, 5, 3, 2} },
     };
 
     bool quit = false;
 
     while (!quit)
     {
-        cout << string(50, '\n');
-        cout << "Fruit Businessman\n";
-        cout << "Day: " << day << endl;
-        printInventory();
-        cout << "------------------------------------\n";
-        printPrice(fruit, 5);
+        printScreen(fruit, 5);
 
         char cmd = ' ';
         while (cmd != 'c' && cmd != 'C')
@@ -89,12 +87,41 @@ int main()
             {
                 sell(fruit, 5);
             }
+
+            //print again
+            printScreen(fruit, 5);
+
         }
-        update();
+        update(fruit, 5);
+
+        if (day >= 60)
+        {
+            printResult();
+            quit = true;
+        }
     }
 
 
     return 0;
+}
+
+void printScreen(Fruit *fruit, int array_size)
+{
+    cout << string(50, '\n');
+    cout << "Fruit Businessman\n";
+    cout << "Month: " << day << endl;
+    cout << getMonthString(day % 12) << ", " << (day - 1) / 12 + 1 << endl;
+    printInventory(fruit, 5);
+    cout << "------------------------------------\n";
+    printPrice(fruit, 5);
+
+}
+
+void printResult()
+{
+    cout << string(50, '\n');
+    cout << "FRUIT BUSINESSMAN\n";
+    cout << "After 5 years, you have : " << networth << "\n";
 }
 
 void printPrice (Fruit fruit[],int n)
@@ -102,31 +129,90 @@ void printPrice (Fruit fruit[],int n)
 		cout <<right<<setw(14)<<"Prices\n";
 		for(int i=0;i<n;i++)
 	{
-		cout <<left<<setw(15)<<fruit[i].name<<"$"<<left<<setw(30)<<fruit[i].price<<"\n";
+		cout << i + 1 << ". " << left<<setw(15) <<fruit[i].name<<"$"<<left<<setw(10)<<fruit[i].price
+        << "last 5 months: " << fruit[i].lastPrices <<"\n";
 	}
 }
 
-void printInventory ()
+void printInventory (Fruit *fruit, int array_size)
 {
-	cout <<right<<setw(15)<<"Inventory\n";
-	cout <<left<<setw(15)<<"Apples: "<<left<<setw(10)<<inv_apple<<"\n"
-		 <<left<<setw(15)<<"Bananas: "<<left<<setw(10)<<inv_banana<<"\n"
-	 	 <<left<<setw(15)<<"Grapes: "<<left<<setw(10)<<inv_grape<<"\n"
-	 	 <<left<<setw(15)<<"Oranges: "<<left<<setw(10)<<inv_orange<<"\n"
-	 	 <<left<<setw(15)<<"Watermelons: "<<inv_watermelon<<"\n"
-		 <<left<<setw(15)<<"Cash: " <<cash<<"\n"
-		 <<left<<setw(15)<<"Days: "<<day<<"\n"
-		 <<left<<setw(15)<<"Networth: "<<networth<<"\n";
+//	cout <<right<<setw(15)<<"Inventory\n";
+//	cout <<left<<setw(15)<<"Apples: "<<left<<setw(10)<<inv_apple<<"\n"
+//		 <<left<<setw(15)<<"Bananas: "<<left<<setw(10)<<inv_banana<<"\n"
+//	 	 <<left<<setw(15)<<"Grapes: "<<left<<setw(10)<<inv_grape<<"\n"
+//	 	 <<left<<setw(15)<<"Oranges: "<<left<<setw(10)<<inv_orange<<"\n"
+//	 	 <<left<<setw(15)<<"Watermelons: "<<inv_watermelon<<"\n"
+//		 <<left<<setw(15)<<"Cash: " <<cash<<"\n"
+//		 <<left<<setw(15)<<"Days: "<<day<<"\n"
+//		 <<left<<setw(15)<<"Networth: "<<networth<<"\n";
+
+    cout << right << setw(15) << "Inventory\n";
+    cout << setprecision(2) << fixed;
+    for (int i = 0; i < array_size; i++)
+    {
+        cout << i + 1 << ". " << left << setw(15) << fruit[i].name << right << setw(3) << fruit[i].amount << " kg" << endl;
+    }
+    cout << "------------------------------------\n";
+    cout << left << setw(15) << "Cash" << "$" << cash << "\n";
+    cout << left << setw(15) << "Networth" << "$" << networth << "\n";
+
 }
 
 bool buy(Fruit* fruit, int array_size)
 {
-    cout << "Bought" << endl;
+    int fruit_type = -1;
+
+    do {
+        fruit_type = getInt("Which one do you want to buy (1-5): ");
+        fruit_type--;
+    } while (fruit_type < 0 || fruit_type > 4);
+
+    float fruit_amount = -1.0f;
+
+    do {
+        fruit_amount = getFloat("How much do you want to buy: ");
+    } while (fruit_amount <= 0);
+
+    float deal_value = fruit[fruit_type].price * fruit_amount;
+    if (cash < deal_value)
+    {
+        cout << "Transaction Failed\n";
+        return false;
+    }
+
+    cash -= deal_value;
+    fruit[fruit_type].amount += fruit_amount;
+    cout << "Transaction Completed\n";
+    return true;
+
 }
 
 bool sell(Fruit* fruit, int array_size)
 {
-    cout << "Sold" << endl;
+    int fruit_type = -1;
+
+    do {
+        fruit_type = getInt("Which one do you want to sell (1-5): ");
+        fruit_type--;
+    } while (fruit_type < 0 || fruit_type > 4);
+
+    float fruit_amount = -1.0f;
+
+    do {
+        fruit_amount = getFloat("How much do you want to sell: ");
+    } while (fruit_amount <= 0);
+
+    if (fruit[fruit_type].amount < fruit_amount)
+    {
+        fruit_amount = fruit[fruit_type].amount;
+    }
+
+    float deal_value = fruit[fruit_type].price * fruit_amount;
+
+    cash += deal_value;
+    fruit[fruit_type].amount -= fruit_amount;
+    cout << "Transaction Completed\n";
+    return true;
 }
 
 char getChar(string question)
@@ -141,16 +227,93 @@ char getChar(string question)
 
 int getInt(string question)
 {
-
+	cout << question;
+	int num;
+	while (!(cin >> num))
+	{
+		cout << question;
+		cin.clear();
+		cin.ignore(9999, '\n');
+	}
+	return num;
 }
 
-void update()
+float getFloat(string question)
 {
-
+    cout << question;
+	float num;
+	while (!(cin >> num))
+	{
+		cout << question;
+		cin.clear();
+		cin.ignore(9999, '\n');
+	}
+	return num;
 }
+
+void update(Fruit *fruit, int array_size)
+{
+	day++;
+	networth = 0;
+	networth += cash;
+	for (int i = 0; i < array_size; i++)
+	{
+        //update depend on month
+		//fruit[i].price += fruit[i].price * 0.05 * getRandom();
+		float rate = fruit[i].months[day % 12];
+		float deltaPrice = fruit[i].price * (0.05 * getRandom() - 0.05 + rate / 100.0f);
+
+        //update price in the last 5 months
+        for (int j = 0; j < 4; j++)
+            fruit[i].lastPrices[j] = fruit[i].lastPrices[j + 1];
+        fruit[i].lastPrices[4] = deltaPrice > 0? '+' : '-';
+
+		fruit[i].price += deltaPrice;
+
+        fruit[i].amount = fruit[i].amount * (100.0 - fruit[i].wastage) / 100.0f;
+
+		networth +=  fruit[i].price *fruit[i].amount;
+	}
+}
+
 
 float getRandom()
 {
     int a = rand();
     return float(( a % 201) - 100 ) / 100;
 }
+
+string getMonthString(int month_as_number)
+{
+    string result;
+    switch (month_as_number)
+    {
+    case 1:
+        return "Junuary";
+    case 2:
+        return "February";
+    case 3:
+        return "March";
+    case 4:
+        return "April";
+    case 5:
+        return "May";
+    case 6:
+        return "June";
+    case 7:
+        return "July";
+    case 8:
+        return "August";
+    case 9:
+        return "September";
+    case 10:
+        return "October";
+    case 11:
+        return "November";
+    case 0:
+        return "December";
+    }
+}
+
+
+
